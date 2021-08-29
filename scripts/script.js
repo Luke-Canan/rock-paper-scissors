313470b0061bf53ec2fcfa526fbda23a369e1281
Luke-Canan <luke.canan@gmail.com>
2021-08-29 13:58:27 -0500

    Modified function playRound to log result of the round AND return a number corresponding to round winner. Also updated comments.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-function computerPlay() { // Outputs computer's move (ROCK, PAPER, or SCISSORS) based on a randomly generated input
+function computerPlay() { // Returns computer's move (ROCK, PAPER, or SCISSORS) based on a randomly generated input
 
     // Generate random integer between 1 and 3 (inclusive)
     let randomInt = Math.floor((Math.random() * 3) + 1);
@@ -19,43 +19,69 @@ function computerPlay() { // Outputs computer's move (ROCK, PAPER, or SCISSORS) 
     return compMove;
 }
 
-function playRound(playerSelection, computerSelection) { // Outputs message displaying results of game
-
-    // Error handling: capitalize user input
-    playerSelection = playerSelection.toUpperCase();
-
-    // Potential output messages - win, lose, or tie
-    let userWins = `You win! ${playerSelection} beats ${computerSelection}.`;
-    let compWins = `You lose. ${computerSelection} beats ${playerSelection}.`;
-    let tie = "Tie.";
-
-    // TIE
+function playRound(playerSelection, computerSelection) { // Logs result of the round AND returns a number corresponding to round winner
+    
+    // Tie
     if (playerSelection === computerSelection) {
-        return tie;
-    } 
-    // PAPER vs rock
-    else if (playerSelection !== "SCISSORS" && computerSelection !== "SCISSORS") {
-        return (playerSelection === "PAPER") ? userWins : compWins;
+        console.log(`Tie. ${playerSelection} ties ${computerSelection}`);
+        return 0;
     }
-    // ROCK vs scissors
-    else if (playerSelection !== "PAPER" && computerSelection !== "PAPER") {
-        return (playerSelection === "ROCK") ? userWins : compWins;
+    // User wins
+    else if (
+        (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+        (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+        (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+    ) {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+        return 1;
     }
-    // SCISSORS vs paper
+    // Computer Wins
     else {
-        return (playerSelection === "SCISSORS") ? userWins : compWins;
+        console.log(`You lose. ${computerSelection} beats ${playerSelection}.`);
+        return 2;
     }
 }
 
-function game() { // Play 5 games
+function game() { // Play 5 games AND display score after each round
+
+    // Score at start of game
+    let tieScore = 0;
+    let userScore = 0;
+    let compScore = 0;
 
     for (let i = 0; i < 5; i++) {
-        // Prompt user for move
-        let player = prompt("Your move: ");
+        // Prompt user for move AND capitalize input
+        let player = prompt("Your move: ").toUpperCase();
         // Generate computer's move
         let computer = computerPlay();
-        // Log result of each round
-        console.log(playRound(player, computer));
+        // Update score
+        switch (playRound(player, computer)) {
+            // Tie
+            case 0: 
+                tieScore++;
+                break;
+            // User wins
+            case 1: 
+                userScore++;
+                break;
+            // Computer wins
+            case 2: 
+                compScore++;
+                break;
+        }
+        // Log updated overall score
+        console.log(`User: ${userScore}, Computer: ${compScore}, Tie: ${tieScore}`);
+    }
+
+    console.log("");
+
+    // Log result of game 
+    if (userScore > compScore) {
+        console.log("You beat the computer!");
+    } else if (compScore > userScore) {
+        console.log("The computer beat you.");
+    } else {
+        console.log("You tied the computer");
     }
 }
 
